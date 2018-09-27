a9148f4e35bc400cd7ce07de9dd1737cfad64a5d
Zhenyu Liu <liuzhenyu44@gmail.com>
2018-09-27 16:41:50 -0400

    done

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
@@ -60,7 +65,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            DevConnector
+            csStudentsSocial
           </Link>
           <button
             className="navbar-toggler"
@@ -76,7 +81,7 @@ class Navbar extends Component {
               <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
                   {' '}
-                  Developers
+                  Students
                 </Link>
               </li>
             </ul>
