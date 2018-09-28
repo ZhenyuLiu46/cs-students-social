a4893ccb6cdd4087dd607715e3c321742be4a218
Zhenyu Liu <liuzhenyu44@gmail.com>
2018-09-27 22:39:02 -0400

    final

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://leo:leo4646@ds157522.mlab.com:57522/devsocialnet',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
